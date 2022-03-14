ba941ee20c5f2abab96d99431a14132b8d66c964
walebot <walesjj@gmail.com>
2022-03-14 17:15:31 +0000

    partial implementation
    still need to clean the backup csv

--- data_newline.py
+++ data_newline.py
@@ -1,19 +1,19 @@
 # Creating newline
-with open('original_data.txt', 'r') as data:
-    with open('clean_data.txt', 'w') as result:
-        for line in data:
-            line = line.replace(',', '\n')
-            result.write(line)
+# with open('original_data.txt', 'r') as data:
+#     with open('clean_data.txt', 'w') as result:
+#         for line in data:
+#             line = line.replace(',', '\n')
+#             result.write(line)
 
 
 
 # Writing comma
-# with open('original_data.txt', 'r') as data:
-#     with open('clean_data.txt', 'w') as result:
-#         for line in data:
-#             if line == '\n':
-#                 result.write(',')
-#             result.write(line)
+with open('original_data.txt', 'r') as data:
+    with open('clean_data.txt', 'w') as result:
+        for line in data:
+            if line == '\n':
+                result.write(',')
+            result.write(line)
 
 
 
